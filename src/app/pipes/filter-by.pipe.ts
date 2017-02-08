d14df2220b5a50838cc7cbf36437bc32f2819718
Benjamin Barbier <benjamin.barbier@symbol-it.fr>
2017-02-08 19:29:07 +0100

    fix: improve filterBy filter about incomplete data

--- src/app/pipes/filter-by.pipe.ts
+++ src/app/pipes/filter-by.pipe.ts
@@ -1,15 +1,15 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import {Pipe, PipeTransform} from '@angular/core';
 import {Link} from "../models/link";
 
 @Pipe({
-  name: 'filterBy'
+    name: 'filterBy'
 })
 export class FilterByPipe implements PipeTransform {
 
-  transform(links: Link[], searchText: string): Link[] {
-    return links.filter((link) => {
-      return link.description.includes(searchText) ||link.title.includes(searchText);
-    });
-  }
+    transform(links: Link[], searchText: string): Link[] {
+        return links.filter((link) => {
+            return (link.description && link.description.includes(searchText)) || (link.title && link.title.includes(searchText));
+        });
+    }
 
 }
